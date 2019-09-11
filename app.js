61069c0bdeab7bc2effdc4506f46d5c41025f51f
Leticia <leticiavna@id.uff.br>
2019-09-11 17:31:25 -0300

    AB#124 cria rotas e páginas de cada animal

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var adoteRouter = require('./routes/adote-especies');
 
 var app = express();
 var hbs = require('hbs');
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/adote', adoteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
