2ba581d96b89ef59531d75ff518afd1244825904
ThalesMachado <thalesmid.uff.br>
2019-09-13 00:21:57 -0300

    Criação do envio de e-mail e do salvamento da adoção no Mongo

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
 const user = process.env.DB_USER
 const password = process.env.DB_PASSWORD
 const url = process.env.DB_URL
 const Cat = mongoose.model('Cat', { name: String });
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'salveosbichin@gmail.com',
+    pass: 'gpms2019'
+  }
+});
 
-mongoose.connect(`mongodb+srv://${user}:${password}@${url}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${user}:${password}@${url}`, { useNewUrlParser: true, dbName: 'AdoptedAnimal' });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -37,7 +47,7 @@ router.get('/adote-uma-especie', function (req, res, next) {
 })
 
 // criar model pessoa
-const personModel = mongoose.model('teste', { nome: String, email: String, nomeEspecie: String });
+const personModel = mongoose.model('AdoptedAnimal', new mongoose.Schema({ name: String, email: String, nomeEspecie: String }));
 
 router.post('/adotar', function (req, res, next) {
   const newPerson = personModel({
@@ -45,6 +55,18 @@ router.post('/adotar', function (req, res, next) {
     email: req.body.emailadote,
     nomeEspecie: req.body.nomeEspecie
   });
+
+  transporter.sendMail(
+    {from: 'salveosbichin@gmail.com',
+      to: req.body.emailadote,
+      subject: 'salveosbichin! | Obrigado por Adotar',
+      text: 'Obrigado, ' + req.body.nomeadote + '! Você Acabou de adotar um(a) ' + req.body.nomeEspecie + '.\n Salveosbichin agradece.'},
+    (err, resp)=>{
+      if (err) console.log(err);
+    });
+  
+
+
   newPerson.save(); // TODO: colocar pra salvar numa collection certinha, de pessoas (por enquanto salva em teste)
   res.render('adote_especies/adote-uma-especie'); // TODO: popup de adotado
 }); // TODO: nao deixar a pessoa adotar outra vez
